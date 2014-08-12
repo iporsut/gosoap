d0dc43ccee5cfa2bf0855bdaa4262670f9c06740
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2014-08-12 14:14:40 +0700

    add parsed message tag

--- wsdl/wsdl.go
+++ wsdl/wsdl.go
@@ -4,9 +4,15 @@ import (
 	"encoding/xml"
 )
 
+type Message struct {
+	XMLName xml.Name `xml:"message"`
+	Name    string   `xml:"name,attr"`
+}
+
 type Definition struct {
-	XMLName       xml.Name `xml:"definitions"`
-	Documentation string   `xml:"documentation"`
+	XMLName       xml.Name  `xml:"definitions"`
+	Documentation string    `xml:"documentation"`
+	Messages      []Message `xml:"message"`
 }
 
 func Unmarshal(b []byte) (definition Definition, err error) {
--- wsdl/wsdl_test.go
+++ wsdl/wsdl_test.go
@@ -17,3 +17,27 @@ func TestUnmarshalDocumentation(t *testing.T) {
 		t.Errorf("expect \"Generated at 08-11-2014 16:33:06:253\" but was %s", definition.Documentation)
 	}
 }
+
+func TestUnmarshalMessage(t *testing.T) {
+	var b, err = ioutil.ReadFile("./testdata/wsdl.xml")
+	if err != nil {
+		t.Error(err)
+	}
+
+	definition, err := Unmarshal(b)
+	if err != nil {
+		t.Error(err)
+	}
+
+	messages := definition.Messages
+
+	if messages[0].Name != "ReadRetlWSInput" {
+		t.Errorf("expect \"ReadRetlWSInput\" but was %s", messages[0].Name)
+	}
+	if messages[1].Name != "ReadRetlWSOutput" {
+		t.Errorf("expect \"ReadRetlWSOutput\" but was %s", messages[0].Name)
+	}
+	if messages[2].Name != "ReadRetlWSError" {
+		t.Errorf("expect \"ReadRetlWSError\" but was %s", messages[0].Name)
+	}
+}
